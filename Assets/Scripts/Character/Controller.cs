76b90af848331ae82bb7412ec58a892f26f6688b
nachomirales <nachomirales@gmail.com>
2017-04-19 09:22:36 +0200

    First Commit

---
+++ Assets/Scripts/Character/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    float velocity = 10.0f;
+    bool mirandoDerecha = true;
+
+    Animator anim;
+    Rigidbody2D miRigidBody;
+
+    void Start()
+    {
+        miRigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+
+	void FixedUpdate () {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        miRigidBody.velocity = new Vector2(moveX * velocity, moveY * velocity);
+        anim.SetFloat("h_velocity", moveX);
+        anim.SetFloat("v_velocity", moveY);
+
+    }
+}
